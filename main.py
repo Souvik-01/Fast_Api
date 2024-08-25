4abd5265ecd14e0c229a11e29bfb5e57e4b27e52
Souvik <joysouvikdas@gmail.com>
2024-08-25 22:02:54 +0530

    new changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from users.routes import router as user_router
 from auth.routes import router as auth_router
@@ -6,6 +6,19 @@ from auth.routes import router as auth_router
 app = FastAPI()
 app.include_router(user_router)
 app.include_router(auth_router)
+
+
+# @app.post('/my-endpoint')
+# async def my_endpoint(stats: Stats, request: Request):
+#     ip = request.client.host
+#     print(ip)
+#     return {'status': 1, 'message': 'ok'}
+
+@app.get("/get-ip")
+async def get_ip(request: Request):
+    client_ip = request.client.host
+    print(client_ip)
+    return {"client_ip": client_ip}
 
 @app.get('/')
 def health_check():
--- users/model.py
+++ users/model.py
@@ -10,7 +10,7 @@ class UserModel(Base):
     first_name = Column(String(20))
     last_name = Column(String(20), nullable=True)
     email = Column(String(20), unique=True, index=True)
-    password = Column(String(50))
+    password = Column(String(100))
     created_at = Column(DateTime, nullable=False, server_default=func.now())
     photo = Column(String(30), nullable=True)
     updated_at = Column(DateTime, nullable=True)
